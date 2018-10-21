043713497d0da0ae06afebd1a448fd40677be2b5
Patiqqqu <patiqqqu@gmail.com>
2018-10-21 21:18:19 +0200

    Initial commit

---
+++ src/main/java/testfx/Controller.java
@@ -0,0 +1,49 @@
+package main.java.testfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private Button plus, minus, multi, div;
+    @FXML
+    private TextField text1, text2;
+    @FXML
+    private TextArea textresult;
+
+    private void clearfields() {
+        text1.clear();
+        text2.clear();
+    }
+
+    @FXML
+    final void addition() {
+        textresult.setText(String.valueOf((Double.parseDouble(text1.getText())) + (Double.parseDouble(text2.getText()))));
+        clearfields();
+    }
+
+    @FXML
+    final void subtraction() {
+        textresult.setText(String.valueOf((Double.parseDouble(text1.getText())) - (Double.parseDouble(text2.getText()))));
+        clearfields();
+    }
+
+    @FXML
+    final void multiplication() {
+        textresult.setText(String.valueOf((Double.parseDouble(text1.getText())) * (Double.parseDouble(text2.getText()))));
+        clearfields();
+    }
+
+    @FXML
+    final void division() {
+        if ((Double.parseDouble(text2.getText())) == 0) {
+            textresult.setText("Nie wolno dzielić przez zero!!!!");
+            clearfields();
+        } else {
+            textresult.setText(String.valueOf((Double.parseDouble(text1.getText())) / (Double.parseDouble(text2.getText()))));
+            clearfields();
+        }
+    }
+}
